ca79d9678d96c556e3ca6454ab0b8f19fde210af
unbaym <unbayahmed@sabanciuniv.edu>
2022-11-01 20:07:56 +0300

    .

---
+++ update.js
@@ -0,0 +1,23 @@
+app.put('/api/stuff/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  userModel.updateOne({id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
